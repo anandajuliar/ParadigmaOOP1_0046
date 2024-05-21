4eb48a397847cc947956b9750788e569298ef435
anandajuliar <anandajulia001@gmail.com>
2024-05-21 20:33:01 +0700

    class pasien

--- asosiasi.cpp
+++ asosiasi.cpp
@@ -4,3 +4,18 @@ using namespace std;
 
 
 class dokter;
+
+class pasien {
+public:
+    string nama;
+    vector<dokter*> daftar_dokter;
+    pasien(string pNama) :nama(pNama) {
+        cout << "Pasien \"" << nama << "\" ada\n";
+
+    }
+    ~pasien() {
+        cout << "Pasien \"" << nama << "\" tidak ada\n";
+    }
+    void tambahDokter(dokter*);
+    void cetakDokter();
+};
