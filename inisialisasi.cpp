10227964a999a38cfbe98ad81f78f9e72db88a1a
anandajuliar <anandajulia001@gmail.com>
2024-05-21 18:06:06 +0700

    publik kelas mahasiswa

--- inisialisasi.cpp
+++ inisialisasi.cpp
@@ -7,3 +7,18 @@ private:
 	const int id; //3 variabel mahasiswa
 	string nama;
 	float nilai;
+
+public:
+	// inisialisasi akan mmeberikan awal nilai variabel melalui konstruktor
+	// membuat konstruktor yg akan membuat nilai awal ketika program di jalankan
+	mahasiswa(int pId, string pNama, float pNilai) : id(pId), nama(pNama), nilai(pNilai) {
+
+	}
+
+	// membuat destruktor
+	~mahasiswa() {
+		cout << "Id = " << id << endl;
+		cout << "Nama = " << nama << endl;
+		cout << "Nilai = " << nilai << endl;
+	}
+};
